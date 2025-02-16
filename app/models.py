cca77d9082399e497fb51c1625b160f46484166c
danielchristy <daaronchristy@gmail.com>
2025-02-15 19:38:29 -0600

    Remade Django Project - New Initial Commit

---
+++ app/models.py
@@ -0,0 +1,70 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Character(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    tags = models.TextField(null=True, blank=True, max_length=255)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    # def tag(self):
+    #     if self.tags:
+    #         return self.tags.split(',')
+    #     return []
+
+    def __str__(self):
+        return self.name
+
+class Session(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    tags = models.TextField(null=True, blank=True, max_length=255)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    # def tag(self):
+    #     if self.tags:
+    #         return self.tags.split(',')
+    #     return []
+
+    def __str__(self):
+        return self.title
+
+class Location(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    tags = models.TextField(null=True, blank=True, max_length=255)
+    date_created = models.DateField(auto_now_add=True)
+
+    # def tag(self):
+    #     if self.tags:
+    #         return self.tags.split(',')
+    #     return []
+    
+    def __str__(self):
+        return self.name
+
+class Event(models.Model):
+    title = models.CharField(max_length=255)
+    session_id = models.ForeignKey(Session, on_delete=models.CASCADE)
+    characters = models.ManyToManyField(Character)
+    summary = models.TextField()
+    location = models.ManyToManyField(Location)
+    tags = models.TextField(null=True, blank=True, max_length=255)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    # def character(self):
+    #     if self.characters:
+    #         return self.characters.split(',')
+    #     return []
+    
+    # def tag(self):
+    #     if self.tags:
+    #         return self.tags.split(',')
+    #     return []
+
+    def __str__(self):
+        return self.id
